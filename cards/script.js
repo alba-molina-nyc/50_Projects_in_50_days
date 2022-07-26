15b094c1bb3a137357da4b9adb0d6db5ca76632e
alba-molina-nyc <avmanyc@gmail.com>
2022-07-26 16:59:52 -0400

    project 1 finished

--- cards/script.js
+++ cards/script.js
@@ -3,9 +3,18 @@ const panels = document.querySelectorAll('.panel')
 panels.forEach((panel) => {
     panel.addEventListener('click', () => {
         console.log('123')
+        removeActiveClasses()
+        panel.classList.add('active')
 
     })
 
   
    
 })
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+
+    })
+}
